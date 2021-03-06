de10ae941a97daab7caacc7a4bcb283fbcefa1af
David Henrich <david.hatl@gmail.com>
2021-03-06 01:18:08 -0300

    Solução boa

--- Reserva_hotel_excecoes/src/application/Program.java
+++ Reserva_hotel_excecoes/src/application/Program.java
@@ -6,43 +6,45 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		System.out.print("Número do quarto: ");
-		int number = sc.nextInt();
-		System.out.print("Data de entrada (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(sc.next());
-		System.out.print("Data de saida: (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(sc.next());
-
-		if (!checkOut.after(checkIn)) {
-			System.out.println("Erro na reserva: a data de saida deve ser depois da data de entrada.");
-		} 
-		else {
+		try{
+			System.out.print("Número do quarto: ");
+			int number = sc.nextInt();
+			System.out.print("Data de entrada (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Data de saida: (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+	
+		
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reserva: " + reservation);
-
+	
 			System.out.println("\nEntre com os dados para atualizar a reserva:");
 			System.out.print("Data de entrada (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Data de saida: (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
-
-			String error = reservation.updateDates(checkIn, checkOut);
-			if(error != null) {
-				System.out.println("Erro na reserva: " + error);
-			}
-			else {
-			System.out.println("Reserva: " + reservation);
-			}
+	
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reserva: " + reservation);		
 		}
-
+		catch (ParseException e) {
+			System.out.println("Data invalida");
+		}
+		catch (DomainException e) {
+			System.out.println("Erro na reserva: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Erro insperado");
+		}
 		sc.close();
 	}
 
--- Reserva_hotel_excecoes/src/model/entities/Reservation.java
+++ Reserva_hotel_excecoes/src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
@@ -12,10 +14,16 @@ public class Reservation {
 	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
-	public Reservation() {
+	public Reservation() throws DomainException {
+		if(! checkOut.after(checkIn)){
+			throw new DomainException("a data de saida deve ser depois da data de entrada.");
+		}
 	}
 
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+		if(! checkOut.after(checkIn)){
+			throw new DomainException("a data de saida deve ser depois da data de entrada.");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -42,19 +50,18 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) throws DomainException {
 		
 		Date now = new Date();
 		if(checkIn.before(now) || checkOut.before(now)) {
-			return "a atualização da reserva deve ser feita para uma data futura à atual.";
+			throw new DomainException("a atualização da reserva deve ser feita para uma data futura.");
 		} 
 		if(! checkOut.after(checkIn)){
-			return "a data de saida deve ser depois da data de entrada.";
+			throw new DomainException("a data de saida deve ser depois da data de entrada.");
 		}
 		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 	}
 
 	@Override
